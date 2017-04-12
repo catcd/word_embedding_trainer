0c0f8ea477142f01550eef2677ef9024f19eb657
Cat Can <ninjameo9x@gmail.com>
2017-04-12 17:25:35 +0700

    update full data

--- parse.py
+++ parse.py
@@ -3,18 +3,22 @@ import os
 
 i = 1
 count = 0
-for fname in os.listdir('/home/catcan/Desktop/ctd_pubmed/ctd_pubmed'):
+elog = open('./error_log', 'w')
+
+for fname in os.listdir('./data/pubmed_xml'):
     try:
-        tree = ET.parse('/home/catcan/Desktop/ctd_pubmed/ctd_pubmed/' + fname)
-        fou = open('/home/catcan/Desktop/data/pubmed/efetch_{}.txt'.format(i), 'w')
+        tree = ET.parse('./data/pubmed_xml/' + fname)
+        fou = open('./data/pubmed_text_only/efetch_{}.txt'.format(i), 'w')
         root = tree.getroot()
         i += 1
         for tag in root.iter('AbstractText'):
-            fou.write((tag.text + '\n').encode('utf-8'))
-            count += 1
+            if tag.text is not None:
+                fou.write((tag.text + '\n').encode('utf-8'))
+                count += 1
+        fou.close()
     except Exception as e:
-        print(fname, i)
-        print(e)
+        elog.write(fname + str(i) + '\n')
+        elog.write(str(e) + '\n')
     else:
         pass
     finally:
